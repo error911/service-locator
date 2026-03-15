dcb8f22e6048629e7d9064577aac77d5794b3108
Roman <murnikr@gmail.com>
2026-03-15 16:15:34 +0300

    sample

--- Assets/GameCore/Services/ServiceLocator/SampleServices/SampleBootstrap.cs
+++ Assets/GameCore/Services/ServiceLocator/SampleServices/SampleBootstrap.cs
@@ -1,3 +1,4 @@
+using System;
 using GameCore.Services;
 using GameCore.Services.Samples;
 using UnityEngine;
@@ -14,6 +15,17 @@ namespace GameCore.Services.SampleServices
             // Регистрируем сервисы по интерфейсам
             ServiceLocator.Register<IAudioService>(_audioService);
             ServiceLocator.Register<ISaveService>(_saveService);
+        }
+
+        private void Start()
+        {
+            // Запуск зарегистрированных сервисов
+            ServiceLocator.Run();
+        }
+
+        private void OnDestroy()
+        {
+            ServiceLocator.Release();
         }
     }
 }
